639929d6da46785dbc4efad7895e20ea001b58f7
rohit <rohitkushwah67838@gmail.com>
2024-09-14 00:32:35 +0530

    Final-change

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
+var menuBtn = document.querySelector("nav h4")
+var fullNav = document.querySelector(" #full-nav")
+
+var flag = 0;
+
+menuBtn.addEventListener("click",()=>{
+  if(flag == 0){
+    fullNav.style.right = 0
+     menuBtn.innerHTML ='<i class="ri-close-large-line"></i>'
+    flag = 1
+  } else{
+    fullNav.style.right = "-100%"
+    menuBtn.innerHTML = '<i class="ri-menu-3-line"></i>'
+    flag = 0
+  }
+  
+})
